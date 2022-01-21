7480829a0e5b3743257b8c2cb44dc65ec4546ff5
mike <mike@htl-steyr.ac.at>
2022-01-21 09:07:46 +0100

    Änderungsvorschläge, damit der Source besser wird!

--- decker.java
+++ decker.java
@@ -1,5 +1,17 @@
+/**
+ * Auch Klassen müssen kommentiert werden
+ * @author mike
+ */
 public class decker {
     public static void main(String[] args) {
+        // TODO Wittner: eine richtige Formatierung wäre schon wichtig
         System.out.println("SuperAlberto35");
+    }
+
+    /**
+     * Kommentieren  nicht vergessen
+     */
+    public void M1(){
+
     }
 }
